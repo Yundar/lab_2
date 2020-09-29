a17fb4242836217a2b12f949fa1356b18c8a26ad
Yurii <Yurri2002@gmail.com>
2020-09-29 23:47:22 +0300

    aaaaa

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ function addDiv(){
     month = '0' + month
   }
   var fullDate = date.getDate() + '.' + month + '.' + date.getFullYear()
-  addingButton.insertAdjacentHTML("beforebegin", '<div class = "note"> \
+  addingButton.insertAdjacentHTML("beforebegin", '<div class = "note" id = "test"> \
+    <button class = "closeBtn" onclick = remDiv()>X</button>\
     <div class = "headline">Заголовок</div>\
     <div class = "time">{Date}</div>\
   </div>'.replace('{Date}', fullDate));
+}
+
+function remDiv(){
+  var test = document.getElementById("test");
+  test.remove()
 }
