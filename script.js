a86bff4b4b50a30e8b1793a0c0dd773f713b75b4
Yurii <Yurri2002@gmail.com>
2020-09-29 22:34:16 +0300

    ...

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function addDiv(){
-  addingButton.insertAdjacentHTML("beforebegin", '<div class = "note"><input></input></div>');
+  var date = new Date();
+  var fullDate = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear()
+  addingButton.insertAdjacentHTML("beforebegin", '<div class = "note"> \
+    <div class = "headline">Заголовок</div>\
+    <div class = "time">{Date}</div>\
+  </div>'.replace('{Date}', fullDate));
 }
