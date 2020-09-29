b5fe8de97d05be7dcc3d14bcda74aa35ba30715a
Yurii <Yurri2002@gmail.com>
2020-09-29 22:40:55 +0300

    /

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function addDiv(){
   var date = new Date();
-  var fullDate = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear()
+  var month = date.getMonth() + 1;
+  if (month <= 9 ){
+    month = '0' + month
+  }
+  var fullDate = date.getDate() + '.' + month + '.' + date.getFullYear()
   addingButton.insertAdjacentHTML("beforebegin", '<div class = "note"> \
     <div class = "headline">Заголовок</div>\
     <div class = "time">{Date}</div>\
